8982d38e80c2acf9fe058a5f207843e5a789697d
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-12-12 13:55:32 -0800

    tested setup, renders

--- src/challengeOne_Profile/profile.js
+++ src/challengeOne_Profile/profile.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Profile = () => {
-	return <div> Do the Thing </div>;
+	const someText = 'New Button Name'
+
+	return <div> Do the Thing 
+		<button>{someText}</button>
+	</div>;
 };
 
 export default Profile;
