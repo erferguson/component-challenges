c4920a0cf55ebfccb07e5ff1bdfdf85c2fbd5089
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-12-14 21:41:02 -0800

    set basic testing counter for friends > tested, passes

--- src/challengeOne_Profile/profile.js
+++ src/challengeOne_Profile/profile.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Profile = () => {
-	const someText = 'New Button Name'
+	const someText = 'Button'
 
-	return <div> Do the Thing 
-		<button>{someText}</button>
-	</div>;
+	const [friends, setFriends] = useState(0)
+
+	const increase = e => {
+		setFriends(friends + 1)
+	}
+
+	return (
+	<div> Do the Thing 
+		<div>New Friends: <span>{friends}</span></div>
+		<button onClick={increase}>{someText}</button>
+	</div>
+	)
 };
 
 export default Profile;
+// <button onClick={ () => submitForm(data) } />
