a29dc7849ea94d1e274332edfadb10ef3a2fdbe0
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-12-16 18:43:49 -0800

    basic rendering of data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Profile from './challengeOne_Profile/profile';
 
 function App() {
+
 	return (
 		<div>
 			<Route exact path="/profile" component={Profile} />
--- src/challengeOne_Profile/profile.js
+++ src/challengeOne_Profile/profile.js
@@ -1,41 +1,97 @@
-import React, {useState} from 'react';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import locationPin from "./assets/pinIcon.svg";
+import msgIcon from "./assets/messageIcon.svg";
+import terminalIcon from "./assets/terminalIcon.svg";
 
-const Profile = (props) => {
-	const someText = 'Code Together'
+const Profile = () => {
+  // state setting the data too
+  const [person, setPerson] = useState(null);
 
-	const [friends, setFriends] = useState(0)
+  const [friends, setFriends] = useState(null);
 
-	const increase = e => {
-		setFriends(friends + 1)
-	}
+  const addFriend = () => {
+    setFriends(friends + 1);
+  };
 
-	return (
-	<div style={{border: '3px solid red'}}> Header 
-		<div style={{border: '3px solid blue'}}>
-			image <br/>
-			name <br/>
-			location <br/>
-		</div>
-		<div style={{border: '3px solid blue'}}>
-			<h3>Friends</h3>
-			<p>some number</p>
-			<h3>Repo</h3>
-			<p>other number</p>
-			<h3>Bugs</h3>
-			<p>third number</p>
-		</div>
-		<div style={{border: '3px solid yellow'}}>
-			<button>
-				<p>small image</p>
-				<p>button title</p>
-			</button>
-			<p>chat image</p>
-		</div>
-	</div>
-	)
+  const deleteFriend = () => {
+    setFriends(friends - 1);
+  };
+
+  useEffect(() => {
+    // calling api
+    axios
+      .get("https://api.github.com/users/erferguson")
+      // do something with data >
+      .then(res => {
+        setPerson(res.data);
+        setFriends(res.data.followers);
+        console.log("data", res);
+      })
+      // if error >
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div style={{ border: "3px solid red" }}>
+      Header
+      <div style={{ border: "3px solid blue" }}>
+        <img src={person.avatar_url} alt="profile photo" /> <br />
+        <p>{person.name}</p>
+        <p>
+          <img src={locationPin} alt="location pin" />
+          {person.location}
+        </p>
+      </div>
+      <div style={{ border: "3px solid blue" }}>
+        <div>
+          <h3>Friends</h3>
+          <p>{person.followers}</p>
+        </div>
+        <div>
+          <h3>Repo</h3>
+          <p>{person.public_repos}</p>
+        </div>
+        <div>
+          <h3>Bugs</h3>
+          <p>{person.public_gists}</p>
+        </div>
+      </div>
+      <div style={{ border: "3px solid yellow" }}>
+        <button
+          onClick={() => {
+            addFriend();
+          }}
+        >
+          <img src={terminalIcon} />
+          <p>Code Together</p>
+        </button>
+        <button
+          onClick={() => {
+            deleteFriend();
+          }}
+        >
+          <img src={terminalIcon} />
+          <p>Code Together</p>
+        </button>
+        <img src={msgIcon} alt="chat icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
 
-{/* <div>Friends: <span>{friends}</span></div>
-		<button onClick={increase}>{someText}</button> */}
+// const someText = 'Code Together'
+
+// 	const [friends, setFriends] = useState(0)
+
+// 	const increase = e => {
+// 		setFriends(friends + 1)
+// 	}
+{
+  /* <div>Friends: <span>{friends}</span></div>
+		<button onClick={increase}>{someText}</button> */
+}
